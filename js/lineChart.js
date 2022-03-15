debbb83b75d0c43749d4c3ec47b9bd2eb69df831
Himanshu Goyal <31973210+HGoyal09@users.noreply.github.com>
2022-03-15 01:47:43 -0700

    line chart

---
+++ js/lineChart.js
@@ -0,0 +1,132 @@
+class LineChart {
+
+    /**
+     * Class constructor with basic chart configuration
+     * @param {Object}
+     * @param {Array}
+     */
+    constructor(_config, _data) {
+        this.config = {
+            parentElement: _config.parentElement,
+            attribute: _config.attribute,
+            containerWidth: _config.containerWidth || 400,
+            containerHeight: _config.containerHeight || 300,
+            margin: _config.margin || {top: 25, right: 20, bottom: 20, left: 35},
+            tooltipPadding: _config.tooltipPadding || 15
+        }
+        this.data = _data;
+        this.initVis();
+    }
+
+    /**
+     * We initialize scales/axes and append static elements, such as axis titles.
+     */
+    initVis() {
+        let vis = this;
+
+        // Calculate inner chart size. Margin specifies the space around the actual chart.
+        vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
+        vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+
+        vis.parseDate = d3.timeParse("%Y");
+        vis.xScale = d3.scaleTime().range([0, vis.width]).domain([vis.parseDate(2011), vis.parseDate(2020)]);
+
+        vis.yScale = d3.scaleLinear()
+            .range([vis.height, 0]);
+
+        // Initialize axes
+        vis.xAxis = d3.axisBottom(vis.xScale)
+            .ticks(10)
+            .tickSize(-vis.height - 10)
+            .tickPadding(10);
+
+        vis.yAxis = d3.axisLeft(vis.yScale)
+            .ticks(6)
+            .tickSize(-vis.width - 10)
+            .tickPadding(10);
+
+        // Define size of SVG drawing area
+        vis.svg = d3.select(vis.config.parentElement)
+            .attr('width', vis.config.containerWidth)
+            .attr('height', vis.config.containerHeight);
+
+        // Append group element that will contain our actual chart
+        // and position it according to the given margin config
+        vis.chart = vis.svg.append('g')
+            .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
+
+        // Append empty x-axis group and move it to the bottom of the chart
+        vis.xAxisG = vis.chart.append('g')
+            .attr('class', 'axis x-axis')
+            .attr('transform', `translate(0,${vis.height})`);
+
+        // Append y-axis group
+        vis.yAxisG = vis.chart.append('g')
+            .attr('class', 'axis y-axis');
+
+        // Append both axis titles
+        vis.chart.append('text')
+            .attr('class', 'axis-title')
+            .attr('y', vis.height - 15)
+            .attr('x', vis.width + 10)
+            .attr('dy', '.71em')
+            .style('text-anchor', 'end')
+            .text('Life Ladder');
+
+        vis.svg.append('text')
+            .attr('class', 'axis-title')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('dy', '.71em')
+            .text(vis.config.attribute);
+    }
+
+    /**
+     * Prepare the data and scales before we render it.
+     */
+    updateVis() {
+        let vis = this;
+
+        vis.xValue = d => {
+            console.log(vis.parseDate(d['year']))
+            return vis.parseDate(d['year'])}
+
+        vis.yValue = d => {
+            console.log(d[vis.config.attribute])
+            return d[vis.config.attribute]}
+
+        // Set the scale input domains
+        vis.yScale.domain([d3.min(vis.data, vis.yValue), d3.max(vis.data, vis.yValue)]);
+
+        vis.renderVis();
+    }
+
+    /**
+     * Bind data to visual elements.
+     */
+    renderVis() {
+        let vis = this;
+
+        vis.line = d3.line()
+            .x(d => vis.xScale(vis.xValue(d)))
+            .y(d => vis.yScale(vis.yValue(d)));
+
+        vis.chart.selectAll('.chart-line')
+            .data([vis.data])
+            .join('path')
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr('class', 'chart-line')
+            .attr('d', vis.line);
+
+        // Update the axes/gridlines
+        // We use the second .call() to remove the axis and just show gridlines
+        vis.xAxisG
+            .call(vis.xAxis)
+            .call(g => g.select('.domain').remove());
+
+        vis.yAxisG
+            .call(vis.yAxis)
+            .call(g => g.select('.domain').remove())
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-let scatterplot;
+let scatterplot, lineplotGdp, lineplotSocial, lineplotLife, lineplotFreedom, lineplotGenerosity, lineplotCorruption;
 let data;
 let scatterplot_attribute = 'Log GDP per capita'
+let country_selected = "Canada"
 
 //Load data from CSV file asynchronously and render chart
 d3.csv('data/happiness_data_with_percentile.csv').then(_data => {
@@ -17,6 +18,60 @@ d3.csv('data/happiness_data_with_percentile.csv').then(_data => {
     }, data);
 
     scatterplot.updateVis();
+
+
+    const filteredData = data.filter(d => d["Country name"] === country_selected ).sort((a, b)  => a.year - b.year)
+    lineplotGdp = new LineChart({
+        parentElement: '#line_chart_gdp',
+        attribute: 'Log GDP per capita'
+        // Optional: other configurations
+    }, filteredData);
+
+
+    lineplotGdp.updateVis();
+
+    lineplotSocial = new LineChart({
+        parentElement: '#line_chart_social',
+        attribute: 'Social support'
+        // Optional: other configurations
+    }, filteredData);
+
+
+    lineplotSocial.updateVis();
+
+    lineplotLife = new LineChart({
+        parentElement: '#line_chart_life',
+        attribute: 'Healthy life expectancy at birth'
+        // Optional: other configurations
+    }, filteredData);
+
+
+    lineplotLife.updateVis();
+
+    lineplotFreedom = new LineChart({
+        parentElement: '#line_chart_freedom',
+        attribute: 'Freedom to make life choices'
+        // Optional: other configurations
+    }, filteredData);
+
+    lineplotFreedom.updateVis();
+
+    lineplotGenerosity = new LineChart({
+        parentElement: '#line_chart_generosity',
+        attribute: 'Generosity'
+        // Optional: other configurations
+    }, filteredData);
+
+    lineplotGenerosity.updateVis();
+
+    lineplotCorruption = new LineChart({
+        parentElement: '#line_chart_corruption',
+        attribute: 'Perceptions of corruption'
+        // Optional: other configurations
+    }, filteredData);
+
+    lineplotCorruption.updateVis();
+
 
 });
 
